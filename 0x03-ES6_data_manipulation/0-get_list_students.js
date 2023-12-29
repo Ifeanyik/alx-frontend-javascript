39081619585c5f92ece07b654de025bf6ae140e6
Matrix <jax64720@gmail.com>
2023-12-29 15:36:56 +0100

    Create student array

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,21 @@
+function getListStudents(){
+    const stud_arr = [
+        {
+            id: 1,
+            firstName: "Guillaume",
+            location: "San Francisco"
+        },
+        {
+            id: 2,
+            firstName: "James",
+            location: "Columbia"
+        },
+        {
+            id: 5,
+            firstName: "Serena",
+            location: "San Francisco"
+        }
+    ];
+    return stud_arr;
+};
+export {getListStudents};
